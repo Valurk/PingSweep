666f5f421afbf51f23cb23cb9beb8a2a10420391
Valurk <73291263+Valurk@users.noreply.github.com>
2022-01-29 22:18:33 -0300

    Update pingSweep.sh

--- pingSweep.sh
+++ pingSweep.sh
@@ -2,20 +2,20 @@
 
 if [ "$1" == "" ]
 
-then 
-
-echo "Usage: ./pingSweep.sh [network]"
-
-echo "example: ./pingSweep.sh 192.168.0"
-
-else 
-
-echo -e "Active hosts\n"
-
-for x in `seq 1 254` ; do
-
-ping -c 1 $1.$x | grep "64 bytes" | cut -d " " -f4 | sed 's/.$//'
-
-done
-
-fi 
+then
+    
+    echo "Usage: ./pingSweep.sh [network]"
+    
+    echo "example: ./pingSweep.sh 192.168.0"
+    
+else
+    
+    echo -e "Active hosts\n"
+    
+    for x in `seq 1 254` ; do
+        
+        ping -c 1 $1.$x | grep "64 bytes" | cut -d " " -f4 | sed 's/.$//'
+        
+    done
+    
+fi
